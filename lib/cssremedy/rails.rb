92e077f798d88ef2389fa12f4581a6699d394666
blakeprudhomme <blake.prudhomme@gmail.com>
2019-02-11 11:23:33 -0600

    Initial commit

---
+++ lib/cssremedy/rails.rb
@@ -0,0 +1,8 @@
+require "cssremedy/rails/version"
+
+module Cssremedy
+  module Rails
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/cssremedy/rails/version.rb
@@ -0,0 +1,5 @@
+module Cssremedy
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
